ef605ece41bd57c49d249ab444a22c3407c836e4
Kenyondra J Conley <kenyondra_conley@yahoo.com>
2019-07-18 03:26:49 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -7,7 +7,11 @@ class Anagram
   
   def match(word_array)
     word_array.select do |word|
-      word.split("").sort == @word.split("").sort 
+      word.split("").sort == @word.split("").sort
+    end
+  end
+end
+
   
     
     
